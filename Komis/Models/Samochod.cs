f0df0982905d37be36078e340dd5cbc72fdc2b53
Student 234968 <234968@student.pwr.edu.pl>
2019-04-26 18:12:30 +0200

    Edit model by add new property and update database - migration

--- Komis/Models/Samochod.cs
+++ Komis/Models/Samochod.cs
@@ -15,6 +15,8 @@
         public string ZdjecieUrl { get; set; }
         public string MiniaturkaUrl { get; set; }
         public bool JestSamochodemTygodnia { get; set; }
+        public bool JestWCentrali { get; set; }
+
 
 
     }
